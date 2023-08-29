90ef74d86b4bf601e31945798e51b17965850d17
Aleksandra Pacia <olusiaop05@gmail.com>
2023-08-29 12:56:41 +0200

    fixed

--- calspace/lib/pages/home_page.dart
+++ calspace/lib/pages/home_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
