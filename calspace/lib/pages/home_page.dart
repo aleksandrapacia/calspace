f6aa8f00bdbe431b69feb80fc65aeba7241b720b
Aleksandra Pacia <olusiaop05@gmail.com>
2023-08-29 12:33:32 +0200

    home page restored

--- calspace/lib/pages/home_page.dart
+++ calspace/lib/pages/home_page.dart
@@ -11,7 +11,19 @@ class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.amber,
+      bottomNavigationBar: GNav(
+          gap: 8,
+          color: Colors.white,
+          activeColor: Colors.white,
+          backgroundColor: Colors.deepPurpleAccent,
+          tabBackgroundColor: Color.fromARGB(255, 157, 121, 255),
+          padding: EdgeInsets.all(16),
+          tabs: [
+            GButton(icon: Icons.home, text: 'Home'),
+            GButton(icon: Icons.calendar_month, text: 'Calendar'),
+            GButton(icon: Icons.article, text: 'Articles'),
+            GButton(icon: Icons.login, text: 'LogIn')
+          ]),
     );
   }
 }
