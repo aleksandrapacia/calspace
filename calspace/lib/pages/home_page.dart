238a8da14ac3c3a5ab73d6400bd59b21133e50bd
Aleksandra Pacia <olusiaop05@gmail.com>
2023-08-29 12:33:32 +0200

    changed colors etc.

--- calspace/lib/pages/home_page.dart
+++ calspace/lib/pages/home_page.dart
@@ -12,12 +12,19 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      bottomNavigationBar: GNav(gap: 8, tabs: [
-        GButton(icon: Icons.home, text: 'Home'),
-        GButton(icon: Icons.calendar_month, text: 'Calendar'),
-        GButton(icon: Icons.article, text: 'Articles'),
-        GButton(icon: Icons.login, text: 'LogIn')
-      ]),
+      bottomNavigationBar: GNav(
+          gap: 8,
+          color: Colors.white,
+          activeColor: Colors.white,
+          backgroundColor: Colors.deepPurpleAccent,
+          tabBackgroundColor: Color.fromARGB(255, 157, 121, 255),
+          padding: EdgeInsets.all(16),
+          tabs: [
+            GButton(icon: Icons.home, text: 'Home'),
+            GButton(icon: Icons.calendar_month, text: 'Calendar'),
+            GButton(icon: Icons.article, text: 'Articles'),
+            GButton(icon: Icons.login, text: 'LogIn')
+          ]),
     );
   }
 }
